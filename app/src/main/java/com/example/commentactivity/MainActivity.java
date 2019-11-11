95d43512d13034a9927c5ec6c206eb82fa1a02b6
quandepzai <43404519+quandepzai@users.noreply.github.com>
2019-11-11 20:54:15 +0700

    aaa

--- app/src/main/java/com/example/commentactivity/MainActivity.java
+++ app/src/main/java/com/example/commentactivity/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.commentactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
